4111eb01b6e474c30a0f6326351ba083f644c5b7
Jake Brooks <jacobdbrooks1998@gmail.com>
2022-04-07 11:46:40 -0500

    added post for locations, employees, and animals

---
+++ views/employee_requests.py
@@ -0,0 +1,68 @@
+EMPLOYEES=[
+        {
+            "id": 1,
+            "name": "Jake Brooks",
+        },
+        {
+            "id": 2,
+            "name": "Erich Hills",
+        },
+        {
+            "id": 3,
+            "name": "Adam Donaghy",
+        },
+        {
+            "id": 4,
+            "name": "Lauden Eller",
+        },
+        {
+            "id": 5,
+            "name": "stevie Hollander",
+        }
+    ]
+
+def create_employee(employee):
+    # Get the id value of the last employee in the list
+    max_id = EMPLOYEES[-1]["id"]
+
+    # Add 1 to whatever that number is
+    new_id = max_id + 1
+
+    # Add an `id` property to the employee dictionary
+    employee["id"] = new_id
+
+    # Add the employee dictionary to the list
+    EMPLOYEES.append(employee)
+
+    # Return the dictionary with `id` property added
+    return employee
+
+def get_all_employees():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
+    return EMPLOYEES
+
+def get_single_employee(id):
+    """_summary_
+
+    Args:
+        id (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    # Variable to hold the found employee, if it exists
+    requested_employee = None
+
+    # Iterate the employeeS list above. Very similar to the
+    # for..of loops you used in JavaScript.
+    for employee in EMPLOYEES:
+        # Dictionaries in Python use  notation to find a key
+        # instead of the dot notation that JavaScript used.
+        if employee["id"] == id:
+            requested_employee = employee
+
+    return requested_employee
